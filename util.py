462c2b76fb1655a9e7da1ccf54dfb6019c2a88fd
Marco Mazzoli <marcomazzoli@kirin.home>
2021-04-25 23:08:00 +0200

    try xgboost on dataset

--- util.py
+++ util.py
@@ -86,3 +86,43 @@ def read_movement_data(path, regex, region='', province=''):
         df = df[df['sub_region_2'] == province]
 
     return df
+
+# # walk-forward validation for univariate data
+# def walk_forward_validation(data, n_test):
+# 	predictions = list()
+# 	# split dataset
+# 	train, test = train_test_split(data, n_test)
+# 	# seed history with training dataset
+# 	history = [x for x in train]
+# 	# step over each time-step in the test set
+# 	for i in range(len(test)):
+# 		# split test row into input and output columns
+# 		testX, testy = test[i, :-1], test[i, -1]
+# 		# fit model on history and make a prediction
+# 		yhat = xgboost_forecast(history, testX)
+# 		# store forecast in list of predictions
+# 		predictions.append(yhat)
+# 		# add actual observation to history for the next loop
+# 		history.append(test[i])
+# 		# summarize progress
+# 		print('>expected=%.1f, predicted=%.1f' % (testy, yhat))
+# 	# estimate prediction error
+# 	error = mean_absolute_error(test[:, -1], predictions)
+# 	return error, test[:, 1], predictions
+
+# # split a univariate dataset into train/test sets
+# def train_test_split(data, n_test):
+# 	return data[:-n_test, :], data[-n_test:, :]
+
+# # fit an xgboost model and make a one step prediction
+# def xgboost_forecast(train, testX):
+# 	# transform list into array
+# 	train = asarray(train)
+# 	# split into input and output columns
+# 	trainX, trainy = train[:, :-1], train[:, -1]
+# 	# fit model
+# 	model = XGBRegressor(objective='reg:squarederror', n_estimators=1000)
+# 	model.fit(trainX, trainy)
+# 	# make a one-step prediction
+# 	yhat = model.predict([testX])
+# 	return yhat[0]
