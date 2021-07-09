0b17eb353fcd8286aea107ebfd5decca411a6c05
Marco Mazzoli <marco.mazzoli.se@gmail.com>
2021-07-09 22:28:21 +0200

    try grangers and johansen tests

--- util.py
+++ util.py
@@ -11,6 +11,9 @@ import os.path, time
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 from xgboost import XGBRegressor
+from statsmodels.tsa.vector_ar.vecm import coint_johansen
+from statsmodels.tsa.stattools import grangercausalitytests
+
 
 def fetch_csv_data(url,date):
     """
@@ -158,3 +161,37 @@ def select_optimal_window_linear(
 def select_time_slot(df,start_date,end_date):
     return df.loc[start_date:end_date]
 
+def grangers_causation_matrix(data,variables,test='ssr_chi2test',verbose=False,maxlag=12):
+    """Check Granger Causality of all possible combinations of the Time series.
+    The rows are the response variable, columns are predictors. The values in the table
+    are the P-Values. P-Values lesser than the significance level (0.05), implies
+    the Null Hypothesis that the coefficients of the corresponding past values is
+    zero, that is, the X does not cause Y can be rejected.
+
+    data      : pandas dataframe containing the time series variables
+    variables : list containing names of the time series variables.
+    """
+    df = pd.DataFrame(np.zeros((len(variables),len(variables))),columns=variables,index=variables)
+    for c in df.columns:
+        for r in df.index:
+            test_result = grangercausalitytests(data[[r, c]], maxlag=maxlag, verbose=False)
+            p_values = [round(test_result[i+1][0][test][1],4) for i in range(maxlag)]
+            if verbose: print(f'Y = {r}, X = {c}, P Values = {p_values}')
+            min_p_value = np.min(p_values)
+            df.loc[r, c] = min_p_value
+    df.columns = [var + '_x' for var in variables]
+    df.index = [var + '_y' for var in variables]
+    return df
+
+def cointegration_test(df,alpha=0.05): 
+    """Perform Johanson's Cointegration Test and Report Summary"""
+    out = coint_johansen(df,-1,1)
+    d = {'0.90':0, '0.95':1, '0.99':2}
+    traces = out.lr1
+    cvts = out.cvt[:, d[str(1-alpha)]]
+    def adjust(val, length= 6): return str(val).ljust(length)
+
+    # Summary
+    print('Name   ::  Test Stat > C(95%)    =>   Signif  \n', '--'*20)
+    for col, trace, cvt in zip(df.columns,traces,cvts):
+        print(adjust(col), ':: ', adjust(round(trace,2), 9), ">", adjust(cvt, 8), ' =>  ' , trace > cvt)
