0cb6565c11e3f7979715149056a6d6c9ceeb3334
Marco Mazzoli <marco.mazzoli.se@gmail.com>
2021-10-22 17:17:10 +0200

    sts single output

--- util.py
+++ util.py
@@ -224,7 +224,27 @@ def grangers_causation_matrix(data, variables, test='ssr_chi2test', verbose=Fals
 # 	return agg
 
 
-def series_to_supervised(data, window=1, lag=1, dropnan=True):
+# def series_to_supervised(data, window=1, lag=1, dropnan=True):
+#     cols, names = list(), list()
+#     # Input sequence (t-n, ... t-1)
+#     for i in range(window, 0, -1):
+#         cols.append(data.shift(i))
+#         names += [('%s(t-%d)' % (col, i)) for col in data.columns]
+#     # Current timestep (t=0)
+#     cols.append(data)
+#     names += [('%s(t)' % (col)) for col in data.columns]
+#     # Target timestep (t=lag)
+#     cols.append(data.shift(-lag))
+#     names += [('%s(t+%d)' % (col, lag)) for col in data.columns]
+#     # Put it all together
+#     agg = pd.concat(cols, axis=1)
+#     agg.columns = names
+#     # Drop rows with NaN values
+#     if dropnan:
+#         agg.dropna(inplace=True)
+#     return agg
+
+def series_to_supervised(data, window=1, lag=1, dropnan=True, column_to_predict=0):
     cols, names = list(), list()
     # Input sequence (t-n, ... t-1)
     for i in range(window, 0, -1):
@@ -234,8 +254,8 @@ def series_to_supervised(data, window=1, lag=1, dropnan=True):
     cols.append(data)
     names += [('%s(t)' % (col)) for col in data.columns]
     # Target timestep (t=lag)
-    cols.append(data.shift(-lag))
-    names += [('%s(t+%d)' % (col, lag)) for col in data.columns]
+    cols.append(data[column_to_predict].shift(-lag))
+    names += [('%s(t+%d)' % (column_to_predict, lag))]
     # Put it all together
     agg = pd.concat(cols, axis=1)
     agg.columns = names
