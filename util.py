c347376d125d0915f448ab8a49001c33e99e9fc1
Marco Mazzoli <marco.mazzoli.se@gmail.com>
2021-11-18 12:01:42 +0100

    fix

--- util.py
+++ util.py
@@ -223,26 +223,6 @@ def grangers_causation_matrix(data, variables, test='ssr_chi2test', verbose=Fals
 # 	return agg
 
 
-# def series_to_supervised(data, window=1, lag=1, dropnan=True):
-#     cols, names = list(), list()
-#     # Input sequence (t-n, ... t-1)
-#     for i in range(window, 0, -1):
-#         cols.append(data.shift(i))
-#         names += [('%s(t-%d)' % (col, i)) for col in data.columns]
-#     # Current timestep (t=0)
-#     cols.append(data)
-#     names += [('%s(t)' % (col)) for col in data.columns]
-#     # Target timestep (t=lag)
-#     cols.append(data.shift(-lag))
-#     names += [('%s(t+%d)' % (col, lag)) for col in data.columns]
-#     # Put it all together
-#     agg = pd.concat(cols, axis=1)
-#     agg.columns = names
-#     # Drop rows with NaN values
-#     if dropnan:
-#         agg.dropna(inplace=True)
-#     return agg
-
 def series_to_supervised(data, window=1, lag=1, dropnan=True, index_to_predict=0):
     cols, names = list(), list()
 
